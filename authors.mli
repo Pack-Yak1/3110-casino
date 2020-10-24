c433594883df0c0fa67113dc72a8e463771c5f2d
Yangwu Wang <yw972@cornell.edu>
2020-10-25 04:10:27 +0800

    Fixed minor fault in authors.mli

--- authors.mli
+++ authors.mli
@@ -1,5 +1,5 @@
 (** CS 3110 Fall 2020 Project
-    @authors Yangwu Wang (yw972), Emily Yi (ety8), Yufan Zhang (yz459) *)
+    @author Yangwu Wang (yw972), Emily Yi (ety8), Yufan Zhang (yz459) *)
 
 (** [hours_worked] is the number of hours you worked on this assignment. *)
 val hours_worked : int
