4db0c75fdd7aa14040c194924a1d20470e55340b
Yufan Zhang <yz459@cornell.edu>
2020-12-19 12:43:35 -0800

    Modified print_hand_result

--- gamestate.ml
+++ gamestate.ml
@@ -184,7 +184,8 @@ let print_player_result name msg p curr =
 (** [print_hand_result players] displays the hand type of all players in 
     [players] in a Poker game. *)
 let print_hand_result st =
-  let players = st.players |> List.filter (fun x -> x.in_game) in
+  let players = st.players |> List.filter 
+                  (fun x -> length x.hand = 5 && x.in_game) in
   for i = 0 to List.length players - 1 do 
     let player = List.nth players i in 
     let result = st.flop.hand |> Deck.concat player.hand |> hand_value in 
